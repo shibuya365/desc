c2bf0636778bf844c2ec43db70aa1a7a9c99358c
shibuya365 <shibuya365days@gmail.com>
2020-09-12 20:22:25 +0900

    add markdown
    add markdown but faild

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,20 @@
 module ApplicationHelper
   def markdown(text)
-    options = {
-      filter_html:     true,
-      hard_wrap:       true,
-      space_after_headers: true,
-      with_toc_data: true
-    }
-
-    extensions = {
-      autolink:           true,
-      no_intra_emphasis:  true,
-      fenced_code_blocks: true,
-      tables:             true
-    }
-
-    renderer = Redcarpet::Render::HTML.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
-    markdown.render(text).html_safe
+    unless @markdown
+      options = {
+        hard_wrap: true
+      }
+      extensions = {
+        no_intra_emphasis: true,
+        tables: true,
+        fenced_code_blocks: true,
+        autolink: true,
+        quote: true
+      }
+      renderer = Redcarpet::Render::HTML.new(options)
+      @markdown = Redcarpet::Markdown.new(renderer, extensions)
+    end
+    @markdown.render(text).html_safe
   end
   
 end
